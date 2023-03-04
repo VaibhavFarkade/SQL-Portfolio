351e1aae3db44c25c6cd683ca9f30b0479b7242a
Vaibhav H. Farkade <99314492+VaibhavFarkade@users.noreply.github.com>
2023-03-04 23:10:33 +0530

    NYTimes Interview Question

--- Project11:Data Lemur/Day04_Laptop vs. Mobile Viewership.sql
+++ Project11:Data Lemur/Day04_Laptop vs. Mobile Viewership.sql
@@ -3,6 +3,6 @@
 --Output the total viewership for laptop and mobile devices in the format of "laptop_views" and "mobile_views".
 
 SELECT 
-  CASE WHEN device_type = 'laptop' THEN 1 ELSE 0 END AS laptop_views, 
-  CASE WHEN device_type IN ('tablet', 'phone') THEN 1 ELSE 0 END AS mobile_views 
+  SUM(CASE WHEN device_type = 'laptop' THEN 1 ELSE 0 END) AS laptop_views, 
+  SUM(CASE WHEN device_type IN ('tablet', 'phone') THEN 1 ELSE 0 END) AS mobile_views 
 FROM viewership;
